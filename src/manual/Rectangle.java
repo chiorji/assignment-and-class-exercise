65c53de88c726015250ec572b724907ec3f9062d
Chigbogu Orji <brightorji60@gmail.com>
2024-08-11 01:54:40 +0100

    chore: course work, rectangle class

--- src/manual/Rectangle.java
+++ src/manual/Rectangle.java
@@ -1,2 +1,27 @@
-package manual;public class Rectangle {
+package manual;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+    
+    public Rectangle(double length, double width){
+        this.setDimensions(length, width);
+    }
+    
+    public double findArea() {
+        return this.length * this.width;
+    }
+    
+    public double findPerimeter() {
+        return (2 * this.length) + (2 * this.width);
+    }
+    
+    public void showDimensions() {
+        System.out.println("Length: " + this.length + ", Width: " + this.width);
+    }
+    
+    public void setDimensions(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
 }
--- src/manual/RectangleTest.java
+++ src/manual/RectangleTest.java
@@ -1,2 +1,20 @@
-package manual;public class RectangleTest {
+package manual;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(3.5, 6);
+        rect.showDimensions();
+        double area = rect.findArea();
+        System.out.println("Area1:" + area);
+        double perimeter = rect.findPerimeter();
+        System.out.println("Perimeter1: " + perimeter);
+        
+        rect.setDimensions(5, 5);
+        rect.showDimensions();
+        area = rect.findArea();
+        System.out.println("Area2: " + area);
+        
+        perimeter = rect.findPerimeter();
+        System.out.println("Perimeter2: " + perimeter);
+    }
 }
